9907103a8d7c939562bd508366575aafc20c6ecf
Suraj Shah <suraj1.shah@gmail.com>
2021-05-05 21:25:23 +0100

    solve power_x_y.py

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    if y < 0:
+        x, y = 1.0 / x, -y
+    result, pow = 1.0, y
+    while pow:
+        if pow & 1:
+            result *= x
+        x, pow = x * x,  pow >> 1
+    return result
 
 
 if __name__ == '__main__':
