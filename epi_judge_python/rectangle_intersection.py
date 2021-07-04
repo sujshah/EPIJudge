d9af7f44856cdc4ee4705604b9966ab63d76458a
Suraj Shah <suraj1.shah@gmail.com>
2021-07-04 19:32:37 +0100

    solve rectangle_intersection.py

--- epi_judge_python/rectangle_intersection.py
+++ epi_judge_python/rectangle_intersection.py
@@ -4,11 +4,36 @@ from test_framework import generic_test
 from test_framework.test_failure import PropertyName
 
 Rect = collections.namedtuple('Rect', ('x', 'y', 'width', 'height'))
+Line = collections.namedtuple('Line', ('x', 'width'))
 
 
 def intersect_rectangle(r1: Rect, r2: Rect) -> Rect:
     # TODO - you fill in here.
-    return Rect(0, 0, 0, 0)
+    lx_1, ly_1 = Line(r1.x, r1.width), Line(r1.y, r1.height)
+    lx_2, ly_2 = Line(r2.x, r2.width), Line(r2.y, r2.height)
+    result = []
+    for l, m in [(lx_1, lx_2), (ly_1, ly_2)]:
+        if l.x > m.x:
+            l,m = m,l
+        if is_overlapping(l, m):
+            result.append(overlapping_line(l,m))
+        else:
+            return Rect(0, 0, -1, -1)
+    return Rect(
+        result[0].x,
+        result[1].x,
+        result[0].width,
+        result[1].width,
+    )
+
+
+def is_overlapping(l1: Line, l2: Line) -> bool:
+    # Assume lines are ordered by bottom-left origin.
+    return l1.x + l1.width >= l2.x
+
+
+def overlapping_line(l1: Line, l2: Line) -> Line:
+    return Line(l2.x, min(l1.width - l2.x + l1.x, l2.width))
 
 
 def intersect_rectangle_wrapper(r1, r2):
