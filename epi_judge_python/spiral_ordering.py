b01d2d5bab705ff7243c084b1ad0bd35a38326cc
Suraj Shah <suraj1.shah@gmail.com>
2021-07-19 21:55:09 +0100

    solve spiral_ordering.py

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -3,9 +3,24 @@ from typing import List
 from test_framework import generic_test
 
 
-def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
+def matrix_in_spiral_order(A: List[List[int]]) -> List[int]:
     # TODO - you fill in here.
-    return []
+    return _matrix_in_spiral_order_reduction(A, len(A), 0)
+
+
+def _matrix_in_spiral_order_reduction(A: List[List[int]], n: int, x: int) -> List[int]:
+    if n == 0:
+        return []
+    if n == 1:
+        return [A[x][x]]
+    if n == 2:
+        return [A[x][x], A[x][x+1], A[x+1][x+1], A[x+1][x]]
+    top_order = A[x][x:x+n-1]
+    bottom_order = list(reversed(A[x+n-1][x+1:x+n]))
+    right_column = [A[y][x+n-1] for y in range(x, x+n-1)]
+    left_column = list(reversed([A[y][x] for y in range(x+1, x+n)]))
+    outer_layer = top_order + right_column + bottom_order + left_column
+    return outer_layer + _matrix_in_spiral_order_reduction(A, n-2, x+1)
 
 
 if __name__ == '__main__':
