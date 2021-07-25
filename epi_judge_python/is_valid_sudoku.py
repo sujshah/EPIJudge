97700a9479cd377ee25e1b11792d153f167b2bd0
Suraj Shah <suraj1.shah@gmail.com>
2021-07-25 17:50:12 +0100

    solve is_valid_sudoku.py

--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -1,4 +1,7 @@
+from math import sqrt
 from typing import List
+
+import numpy as numpy
 
 from test_framework import generic_test
 
@@ -6,7 +9,21 @@ from test_framework import generic_test
 # Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment: List[List[int]]) -> bool:
     # TODO - you fill in here.
+    n = len(partial_assignment)
+    if any(contains_duplicates(x) for x in partial_assignment):
+        return False
+    if any(contains_duplicates([partial_assignment[i][j] for i in range(n)]) for j in range(n)):
+        return False
+    l = int(sqrt(n))
+    if any(contains_duplicates([partial_assignment[i + I][j + J] for i in range(l) for j in range(l)]) for I in
+           range(0, n, l) for J in range(0, n, l)):
+        return False
     return True
+
+
+def contains_duplicates(block: List[int]) -> bool:
+    block = list(filter(lambda x: x != 0, block))
+    return len(block) != len(set(block))
 
 
 if __name__ == '__main__':
