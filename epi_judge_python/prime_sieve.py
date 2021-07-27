5137badd0739ce8937c939ec77b29fd215adca3e
Suraj Shah <suraj1.shah@gmail.com>
2021-07-27 22:27:39 +0100

    solve prime_sieve.py

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -6,7 +6,14 @@ from test_framework import generic_test
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
     # TODO - you fill in here.
-    return []
+    result = []
+    primes = [0, 0] + [1]*(n-1)
+    for i in range(2, n+1):
+        if primes[i]:
+            result.append(i)
+            for j in range(i*i, n+1, i):
+                primes[j] = 0
+    return result
 
 
 if __name__ == '__main__':
