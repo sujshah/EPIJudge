caf97000635eb4b19ac68765caad42f5b3bfa702
Suraj Shah <suraj1.shah@gmail.com>
2021-08-09 22:06:04 +0100

    test github token

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -18,7 +18,6 @@ def is_palindromic(s: str) -> bool:
 
 
 if __name__ == '__main__':
-    print("1")
     exit(
         generic_test.generic_test_main('is_string_palindromic.py',
                                        'is_string_palindromic.tsv',
