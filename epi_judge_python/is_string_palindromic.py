1347cba56c8d35555314d06a2791ea4df9c72a65
Suraj Shah <suraj1.shah@gmail.com>
2021-08-09 21:06:53 +0100

    solve is_string_palindromic.py

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -3,6 +3,17 @@ from test_framework import generic_test
 
 def is_palindromic(s: str) -> bool:
     # TODO - you fill in here.
+    i, j = 0, len(s) -1
+    while i < j:
+        if not s[i].isalnum():
+            i += 1
+            continue
+        if not s[j].isalnum():
+            j -= 1
+            continue
+        if s[i] != s[j]:
+            return False
+        i, j = i + 1, j - 1
     return True
 
 
