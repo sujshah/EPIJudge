8d230f2984ce1537173a2776e87536be5f1c07c0
Suraj Shah <suraj1.shah@gmail.com>
2021-08-11 21:18:21 +0100

    solve look_and_say.py

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -3,7 +3,20 @@ from test_framework import generic_test
 
 def look_and_say(n: int) -> str:
     # TODO - you fill in here.
-    return ''
+    s = "1"
+    for _ in range(n - 1):
+        s = look_and_say_iteration(s)
+    return s
+
+
+def look_and_say_iteration(s: str) -> str:
+    start, counter, result = 0, 0, []
+    while counter < len(s):
+        while counter < len(s) and s[start] == s[counter]:
+            counter += 1
+        result.extend([str(counter - start), s[start]])
+        start = counter
+    return "".join(result)
 
 
 if __name__ == '__main__':
