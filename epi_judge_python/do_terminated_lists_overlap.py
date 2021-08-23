048f8f9a32ff87c615d3df2d12847346fa781b7a
Suraj Shah <suraj1.shah@gmail.com>
2021-08-23 22:08:19 +0100

    solve do_terminated_lists_overlap.py

--- epi_judge_python/do_terminated_lists_overlap.py
+++ epi_judge_python/do_terminated_lists_overlap.py
@@ -1,4 +1,5 @@
 import functools
+from typing import Optional
 
 from list_node import ListNode
 from test_framework import generic_test
@@ -6,9 +7,41 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def overlapping_no_cycle_lists(l0: ListNode, l1: ListNode) -> ListNode:
+def overlapping_no_cycle_lists(l0: ListNode, l1: ListNode) -> Optional[ListNode]:
     # TODO - you fill in here.
-    return ListNode()
+    step_0, step_1 = 0, 0
+    dummy_0, dummy_1 = ListNode(0, l0), ListNode(0, l1)
+    while dummy_0.next is not None:
+        step_0 += 1
+        dummy_0 = dummy_0.next
+    while dummy_1.next is not None:
+        step_1 += 1
+        dummy_1 = dummy_1.next
+    if dummy_0 is dummy_1:
+        longer, shorter = (l1, l0) if step_1 > step_0 else (l0, l1)
+        for _ in range(abs(step_1 - step_0)):
+            longer = longer.next
+        while longer is not shorter:
+            longer, shorter = longer.next, shorter.next
+        return shorter
+    return None
+
+
+def overlapping_no_cycle_lists_clean(l0: ListNode, l1: ListNode) -> ListNode:
+    def length(L: ListNode) -> int:
+        length = 0
+        while L:
+            length += 1
+            L = L.next
+        return length
+    len_1, len_0 = length(l0), length(l1)
+    if len_1 < len_0:
+        l0, l1 = l1, l0
+    for _ in range(abs(len_1 - len_0)):
+        l0 = l0.next
+    while l0 and l1 and l0 is not l1:
+        l0, l1 = l0.next, l1.next
+    return l0
 
 
 @enable_executor_hook
