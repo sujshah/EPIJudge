8bf98383c252fafafac436bb5b44c7b72eebdcc0
Suraj Shah <suraj1.shah@gmail.com>
2021-09-02 21:57:40 +0100

    solve evaluate_rpn.py

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -3,7 +3,22 @@ from test_framework import generic_test
 
 def evaluate(expression: str) -> int:
     # TODO - you fill in here.
-    return 0
+    elements = expression.split(",")
+    stack = []
+    for e in elements:
+        if e.isnumeric():
+            stack.append(int(e))
+        else:
+            b, a = int(stack.pop()), int(stack.pop())
+            if e == "+":
+                stack.append(a + b)
+            elif e == "/":
+                stack.append(a // b)
+            elif e == "*":
+                stack.append(a * b)
+            elif e == "-":
+                stack.append(a - b)
+    return int(stack.pop())
 
 
 if __name__ == '__main__':
